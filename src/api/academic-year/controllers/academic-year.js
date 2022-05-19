711c8ea4a3e012d266702ace530ba10eab23acb9
AlaaOsama25 <alaaosamazaki@gmail.com>
2022-05-19 05:00:25 +0200

    updates

--- src/api/academic-year/controllers/academic-year.js
+++ src/api/academic-year/controllers/academic-year.js
@@ -1,15 +1,9 @@
-const qs = require('qs');
-const query = qs.stringify( async, {
-  populate: {
-    graduated_number: {
-      populate: '*',
-      //populate: ['IS'],
-    },
-    
-  } 
-}, {
-  encodeValuesOnly: true,
-});
+'use strict';
 
-await request(`/api/academic-years?${query}`);
-// GET /api/academic-years?populate[graduated_number][populate][0]=CS
+/**
+ *  academic-year controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::academic-year.academic-year');
