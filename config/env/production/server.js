526e856d3da9a747027266deeec1a3b315997769
AlaaOsama25 <alaaosamazaki@gmail.com>
2022-05-20 22:40:37 +0200

    config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,5 +3,6 @@ module.exports = ({ env }) => ({
     url: env('MY_HEROKU_URL'),
     app: { 
       keys: env.array('APP_KEYS', ['3eQCr3ttlKki71cWlAxirQ==','0bdoBJYF5+mfG8wpsyRu8g==']),
+      
     },
   })
