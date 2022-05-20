387e4a019f30dc1fef3d200868fd20fd88c795fe
AlaaOsama25 <alaaosamazaki@gmail.com>
2022-05-20 23:16:41 +0200

    edit

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -3,10 +3,16 @@
 module.exports = ({ env }) => ({
     // enable a plugin that doesn't require any configuration
     i18n: true,
+    'users-permissions': {
+        config: {
+          jwt: {
+            expiresIn: '7d',
+          },
+          jwtsecret: '9mlygbGZbaiTCH4BjsgmMw=='
+        },
+      },
   
-    // enable a custom plugin
-   
+    
   
     
   });
-  
--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,9 @@ module.exports = ({ env }) => ({
       config: {
         jwt: {
           expiresIn: '7d',
+          
         },
+        jwtsecret: '9mlygbGZbaiTCH4BjsgmMw=='
       },
     },
     // ...
