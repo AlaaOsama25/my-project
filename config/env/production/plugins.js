7069838d1587f85979df380c3099bb56eee7dbbe
AlaaOsama25 <alaaosamazaki@gmail.com>
2022-05-20 22:57:08 +0200

    update

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -5,18 +5,8 @@ module.exports = ({ env }) => ({
     i18n: true,
   
     // enable a custom plugin
-    myplugin: {
-      // my-plugin is going to be the internal name used for this plugin
-      enabled: true,
-      resolve: './src/plugins/my-local-plugin',
-      config: {
-        // user plugin config goes here
-      },
-    },
+   
   
-    // disable a plugin
-    myotherplugin: {
-      enabled: false, // plugin installed but disabled
-    },
+    
   });
   
