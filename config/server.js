799b01c4770ddf736ac75d36e05310726ab24676
AlaaOsama25 <alaaosamazaki@gmail.com>
2022-05-21 14:43:24 +0200

    update

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['3eQCr3ttlKki71cWlAxirQ==','0bdoBJYF5+mfG8wpsyRu8g==']),
+    keys: env.array('APP_KEYS'),
   },
 });
